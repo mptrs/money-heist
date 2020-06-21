f2f7e745e6b0299cd4900543e5706ec5e029d55f
Michiel Peters <wishjuh@gmail.com>
2020-06-21 16:17:53 +0200

    Updates

---
+++ precache-manifest.91c6dc61797c50d3423dab4d76ca739a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a3768fcf1f165945cfc96f85db2c3f",
+    "url": "/money-heist/index.html"
+  },
+  {
+    "revision": "528b0e241c434a58334d",
+    "url": "/money-heist/static/css/main.14516590.chunk.css"
+  },
+  {
+    "revision": "f74cf3333e12d6daf059",
+    "url": "/money-heist/static/js/0.38db6572.chunk.js"
+  },
+  {
+    "revision": "8ca683d0b6550fc9d281",
+    "url": "/money-heist/static/js/3.c5329e4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/money-heist/static/js/3.c5329e4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "528b0e241c434a58334d",
+    "url": "/money-heist/static/js/main.511bc7a2.chunk.js"
+  },
+  {
+    "revision": "9f10816271f85623b71b",
+    "url": "/money-heist/static/js/runtime-main.8e35cc84.js"
+  }
+]);
